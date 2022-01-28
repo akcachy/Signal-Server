5e22fd503a799b411db05284acc2e0b1a122ea80
Ankit Kumar <ankit.kumar@cachy.com>
2022-01-28 12:39:05 +0530

    is story flag

--- service/src/main/java/org/whispersystems/textsecuregcm/entities/CachyUserPostResponse.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/entities/CachyUserPostResponse.java
@@ -66,7 +66,7 @@ public class CachyUserPostResponse {
     @JsonProperty
     public int dirtyFlag;
 
-    @JsonIgnore
+    @JsonProperty
     public boolean isStory;
 
     public CachyUserPostResponse() {
